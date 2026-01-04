76668fa9861b0bbfe35658fb448b7a29bf6582d7
Martin Mahner <martin@mahner.org>
2026-01-04 09:17:56 +0100

    Increase build timeout

--- CF Cache Status/CF Cache Status/AppDelegate.swift
+++ CF Cache Status/CF Cache Status/AppDelegate.swift
@@ -5,17 +5,5 @@
 //  Created by Martin Mahner on 02.01.26.
 //
 
-import Cocoa
-
-@main
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        // Override point for customization after application launch.
-    }
-
-    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        return true
-    }
-
-}
+// AppDelegate.swift is no longer needed - using SwiftUI App lifecycle
+// See CacheStatusApp.swift
